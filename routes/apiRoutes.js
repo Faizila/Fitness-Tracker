513d4352acc6e74cffc1113fe64c232fd16db02f
Faizila Amlani <amlani.faizila@gmail.com>
2021-11-11 17:34:35 -0600

    setting up routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,11 @@
+// import
+const path = require("path");
+const router = require("express").Router();
+const models = require("../models");
+
+
+
+
+
+//export
+module.exports = router;
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,11 @@
+// import
+const path = require("path");
+const router = require("express").Router();
+
+
+
+
+
+
+//export
+module.exports = router;

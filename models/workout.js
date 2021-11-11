54e49c2a7d7d94606e9d1834d6d4450bd8cac3e6
Faizila Amlani <amlani.faizila@gmail.com>
2021-11-11 16:55:57 -0600

    setting up schema in workout.js

--- models/workout.js
+++ models/workout.js
@@ -6,7 +6,36 @@ const Schema = mongoose.Schema;
 // Create workout schema
 const WorkoutSchema = new Schema({
 
-  
+day: {
+    type: Date,
+    default: Date.now
+},
+
+exercises: [{
+    type: { 
+    type: String,
+    },
+    name: { 
+      type: String,
+    },
+    distance: { 
+      type: Number
+     },
+    duration: { 
+      type: Number,
+     },
+    weight: { 
+      type: Number
+     },
+    sets: { 
+      type: Number
+     },
+    reps: { 
+      type: Number
+     }
+    
+}]
+
 });
 
 // create model Workout

410229ac8943afb5d2a37fb47b6f6d55c971d53d
Faizila Amlani <amlani.faizila@gmail.com>
2021-11-11 18:19:06 -0600

    setting api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,36 @@
 // import
-const path = require("path");
 const router = require("express").Router();
-// const models = require("../models");
+const db = require("../models/workoutSchema.js");
 
+// routes
 
+// GET 
+router.get("/api/workouts", (req, res) => {
+	    // find all
+        db.find()
+        // promise
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+        // error
+		.catch(err => {
+            res.status(400).json(err);
+		});
+});
 
-
+// POST 
+router.post("/api/workouts", ({ body }, res) => {
+      // create   
+      db.create(body)
+     // promise   
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      // error  
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 
 //export
 module.exports = router;
